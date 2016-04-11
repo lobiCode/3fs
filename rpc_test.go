639e1cd9b3b6b4d484d5e52fdf5daa9710c73451
Uros Slana <urossla@gmail.com>
2016-04-11 23:44:04 +0200

    add TestFR

--- rpc_test.go
+++ rpc_test.go
@@ -57,3 +57,39 @@ func TestSI(t *testing.T) {
 		t.Error(errors.New(test + "wrong country identifier"))
 	}
 }
+
+func TestFR(t *testing.T) {
+
+	var reply mylib.MsisdnFormat
+
+	test := "TestFR "
+	msisdn := "+33123456789"
+	cc := "33"
+	ndc := ""
+	ndc1 := "1"
+	sn := "23456789"
+	sn1 := "123456789"
+	ci := "FR"
+
+	err := rpcClient.Call(method, msisdn, &reply)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if reply.Cc != cc {
+		t.Error(errors.New(test + "wrong country dialing code"))
+	}
+
+	if reply.Ndc != ndc && reply.Ndc != ndc1 {
+		t.Error(errors.New(test + "wrong mno identifier"))
+	}
+
+	if reply.Sn != sn && reply.Sn != sn1 {
+		t.Error(errors.New(test + "wrong sucriber identifier"))
+	}
+
+	if reply.Ci != ci {
+		t.Error(errors.New(test + "wrong country identifier"))
+	}
+}
